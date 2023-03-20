cb6edc051052e692d4ee0e3f976bc4e7a35d9515
talekunci <smouke.king1@gmail.com>
2023-03-20 11:34:26 +0200

    The first task done.

--- src/main/java/ua/cpu/Main.java
+++ src/main/java/ua/cpu/Main.java
@@ -2,7 +2,19 @@ package ua.cpu;
 
 public class Main {
 
+//    The first task
     public static void main(String[] args) {
+        byte time = 11, speed = 37;
+
+        int distance = time * speed;
+
+        System.out.printf("time = %d,%n" +
+                        "speed = %d,%n" +
+                        "distance = %d%n",
+                time,
+                speed,
+                distance
+        );
 
     }
 
